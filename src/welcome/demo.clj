84f6686adba907332242e71318f551784c55f7fe
Bill La Forge <laforge49@gmail.com>
2016-09-01 12:32:36 -0400

    stubbed profile

--- src/welcome/demo.clj
+++ src/welcome/demo.clj
@@ -3,7 +3,10 @@
             [welcome.server]))
 
 (users/add-user! "Fred" "fred" {:welcome {:full-name "Freddy Krueger"}
+                                :profile {}
                                 :contacts {}})
 (users/add-user! "Sam" "sam" {:welcome {:full-name "Sam I Am"}
+                              :profile {}
                               :contacts {}})
-(users/add-user! "Kris" "kris" {:welcome {:full-name "Kris Kringle"}})
+(users/add-user! "Kris" "kris" {:welcome {:full-name "Kris Kringle"}
+                                :profile {}})
